5cefa721c209f7e7884dad7ad75a0e718d5448da
Gabriella Martins <gabmcavalheiro641@gmail.com>
2022-03-25 17:49:50 -0300

    Terminamos a apostila 03, criando um componente React e estudando sobre JSX

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './style.css'
 
 const App = () => {
-    return <div> Meu primeiro componente ReactJS </div>
+    const estiloBotao = {
+        marginTop: 12,
+        paddingTop: 8,
+        paddingBottom: 8,
+        backgroundColor: 'blueviolet',
+        color: 'white',
+        border: 'none',
+        width: '100%',
+        borderRadius: 8
+    }
+    const textoDoRotulo = 'Nome'
+    const obterTextoBotao = () => "Enviar"
+    // const obterTextoBotaocomReturn = () => {
+    //     return "Enviar"
+    // }
+    return( 
+        <div style={{margin: 'auto', width: 768, backgroundColor:
+        '#EEE', padding: 12, boderRadius: 8}}>
+            <label className="rotulo" htmlFor="nome" style={{display: 'block', marginBottom: 4}}>{textoDoRotulo}</label>
+        <input id="nome" type="text" style={{paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden', width: '100%',
+        borderRadius: 8, outline: 'none'}}></input>
+        <button style={estiloBotao}>{obterTextoBotao()}</button>
+        
+        </div>
+    )
 }
 
 ReactDOM.render( 
     <App />,
-    document.querySelector('#root')
+    //document.ElementById('root') pelo id
+    document.querySelector('#root') // por vários seletores
 )
